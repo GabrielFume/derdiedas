37c5e47e78f9b8a0481c7b3d0c910ae2c22c7019
Gabriel <gabrielgac75@gmail.com>
2018-02-27 00:29:50 -0400

    nuevo home y nueva seccion de partners

---
+++ assets/logos/Document_files/home.js
@@ -0,0 +1,15 @@
+ $(document).ready(function() {
+    $('select').material_select();
+  });
+
+//  // Can also be used with $(document).ready()
+// $(window).load(function() {
+//   $('.flexslider').flexslider({
+//     animation: "slide",
+//     rtl: true
+//   });
+// });
+
+$(document).ready(function(){
+              $('.slider').slider({full_width: true});
+            });
