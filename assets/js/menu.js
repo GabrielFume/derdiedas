42b96fb2ce3aa9ac89220cd5987376e8da315102
Gabriel <gabrielgac75@gmail.com>
2018-03-22 16:59:43 -0400

    Menu dinamico

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+$(function(){
+
+var header = document.getElementById("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(header);
+// initialise
+headroom.init();
+
+});
+
